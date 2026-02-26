68dac4062fb79f5966eebddec02fc085125ffd29
chorouqla <chorouq.lagmani@estudiant.fjaverianas.com>
2026-02-26 10:55:32 +0100

    Add files via upload

---
+++ verticalPlatformer/script.js
@@ -0,0 +1,96 @@
+const canvas = document.querySelector("canvas")
+const c = canvas.getContext("2d")
+
+canvas.width = 1024
+canvas.height = 576
+
+const scaledCanvas = {
+    width: canvas.width / 2,
+    height: canvas.height / 2,
+}
+
+const gravity = 0.5
+
+class Sprite {
+    constructor({ position, imageSrc }) {
+        this.position = position
+        this.image = new Image()
+        this.image.src = imageSrc
+    }
+
+    draw() {
+        if (!this.image) return
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+    update() {
+        this.draw()
+    }
+}
+
+class Player {
+    constructor(position) {
+        this.position = position
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
+        this.height = 100
+    }
+
+    draw() {
+        c.fillStyle = "red"
+        c.fillRect(200, this.position.y, 100, 100)
+    }
+
+    update() {
+        this.draw()
+        this.position.y += this.velocity.y
+        this.position.x += this.velocity.x
+        if (this.position.y + this.height + this.velocity.y < canvas.height)
+            this.velocity.y += gravity
+        else this.velocity.y = 0
+    }
+}
+
+const player = new Player({
+    x: 0,
+    y: 0,
+})
+
+const player2 = new Player({
+    x: 300,
+    y: 100,
+})
+
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0,
+    },
+    imageSrc: "./png/background.png"
+})
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = "white"
+    c.fillRect(0, 0, canvas.width, canvas.height)
+
+    c.save()
+    c.scale(2, 2)
+    c.translate(0, -background.image.height + scaledCanvas.height)
+    background.update()
+    c.restore()
+
+    player.draw()
+    player.update()
+}
+
+animate()
+
+window.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "rightArrow":
+            player.velocity.x = 1
+            break
+    }
+})
